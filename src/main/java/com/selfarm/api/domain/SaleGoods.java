3b79893bbc33bc379732adaae879ff88842ef01d
Cho ByeongChan <jobc0335@gmail.com>
2018-01-27 17:43:11 +0900

    add sale goods domain

---
+++ src/main/java/com/selfarm/api/domain/SaleGoods.java
@@ -0,0 +1,54 @@
+package com.selfarm.api.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by ByeongChan on 2018. 1. 27..
+ */
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(value = { AuditingEntityListener.class })
+@Table(name="sale_goods")
+public class SaleGoods {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long sgid;
+
+    @Column
+    private String name;
+
+    @Column
+    private int sales_price;
+
+    @Column
+    private int sales_amount;
+
+    @Column(insertable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date harvest_date;
+
+    @Column
+    private String image;
+
+    public static SaleGoods build(String name, int sales_price, int sales_amount, Date harvest_date, String image) {
+        return SaleGoods.builder()
+                .name(name)
+                .sales_price(sales_price)
+                .sales_amount(sales_amount)
+                .harvest_date(harvest_date)
+                .image(image)
+                .build();
+    }
+}
